f7d146d7c781309c0e862e2f0615f27f86162f30
tobiaspasinato <tobiasmarpasinato2005@gmail.com>
2024-03-20 01:34:15 -0300

    asd

--- flows/flow-sobre-mi.js
+++ flows/flow-sobre-mi.js
@@ -1,13 +1,15 @@
 import bot from "@bot-whatsapp/bot"
-import flowPrincipal from "./flowprincipal"
+import flowPrincipal from "/Users/midlesus/Desktop/prog/0800WEB/chatbot/flows/flowprincipal.js";
 
 const flow_sobre_mi = bot.addKeyword("sobre mi").addAnswer(
     [
         'texto sobre mi que me dio paja hacer',
+        'Para volver al menu principal escribi "back"',
+        'Para dejar de hablar escribi "off"'
     ],
     async(ctx,[gotoFlow, endFlow]) => {
-        if(ctx.body.toLowerCase() == "back"){
-            await gotoFlow(flowPrincipal)
+        if(ctx.body.toLowerCase() == 'back'){
+            return await gotoFlow(flowPrincipal)
         } else if (ctx.body.toLowerCase() == 'off'){
             return endFlow({body:'Que la fuerza te acompañe'})
         }
--- flows/flowprincipal.js
+++ flows/flowprincipal.js
@@ -5,10 +5,16 @@ const flowPrincipal = bot.addKeyword('asd').addAnswer(
     [
         'Hola estas comunicandote con el esclado personal de tobi',
         'Digo digo, el bot personal de tobi. En que puedo ayudarte?',
+        ' ',
         'Para pedir mas información sobre mi mande "sobre mi"',
+        ' ',
         'Para saber como ser feliz mande "ser feliz"',
+        ' ',
+        'Fran pone "secreto"',
+        ' ',
         'Que es lo que compartimos todos los dias? (escribir solo la palabra)',
-        'Para salir mande "cancel',
+        ' ',
+        'Para salir mande "off"',
     ],
     {capture:true, delay : 2000}, 
     async (ctx,{flowDynamic, gotoFlow, endFlow}) => {
@@ -18,6 +24,10 @@ const flowPrincipal = bot.addKeyword('asd').addAnswer(
             await gotoFlow(flow_sobre_mi)
         } else if(ctx.body.toLowerCase() == "relicario"){
             return flowDynamic('Te amo mi amor 💖')
+        } else if (ctx.body.toLowerCase() == "secreto"){
+            return flowDynamic('Sele se la come doblada xdxdxd')
+        } else if (ctx.body.toLowerCase() == 'off'){
+            return endFlow({body:'Que la fuerza te acompañe'})
         } else {
             return flowDynamic('No tengo ese comando definido')
         }
